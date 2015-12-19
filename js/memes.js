559f4628bf3816b543b5608ae4a7c7211117af70
dwaindibbly <ajclarke903@gmail.com>
2015-12-19 14:08:09 +0000

    Very dank page now, with new snoop dogg

--- js/memes.js
+++ js/memes.js
@@ -6,31 +6,41 @@ function mememe (type , text , img) {
 	this.type = type;
 	this.text = text;
 	this.img = img;
-	this.number = counting("up" , 1 , numberofmeme);
+	this.number = counting("up" , 1 , numberofmeme , true);
 };
-function counting(mode , interval , variable) {
+function counting(mode , interval , variable , getin) {
 	if (mode === "reset") {
-		this.variable = 0;
+		variable = 0;
 	};
 	if (mode === "up") {
-		this.variable = this.variable + interval;
+		variable = variable + interval;
 	};
 	if (mode === "down") {
-		this.variable = this.variable - interval;
+		variable = variable - interval;
 	};
-	return this.variable
+	if (getin === true) {
+	return variable;
+	};
 };
 
-function memewriter (I) {
-	
-}
+
 
 function appendtoarray(array, variable) {
+	array.push(variable);
+};
+
+function xmlvarbranch(branch) {
 	
 };
 
 var sanic = new mememe("fast","Cum on step it up!","img/Sanic.png");
-var snoopdogg = new mememe("dank");
+var snoopdogg = new mememe("dank","smoke weed every day","img/snoopdogg.jpeg");
+
+function memewriter (I) {
+	var line = "<p>" + I.type + "= <img src=" + I.img + " alt=" + I.text + "></img></p>";
+	document.getElementById("memes") = document.getElementById("memes") + line;
+	
+}
 
 function pooptest() {
 	
