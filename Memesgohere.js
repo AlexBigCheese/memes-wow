f753512a8615910411a53849fa916d64a1a04e52
dwaindibbly <ajclarke903@gmail.com>
2015-12-20 15:04:47 +0000

    uncapsd moar snoog (D)ogg stuff

--- Memesgohere.js
+++ Memesgohere.js
@@ -1,4 +1,4 @@
 var sanic = new mememe("fast" , "cum on step it up", "img/Sanic.png");
-var snoopDogg = new mememe("dank", "smoke weed every day", "img/snoopdogg.jpeg");
+var snoopdogg = new mememe("dank", "smoke weed every day", "img/snoopdogg.jpeg");
 var dankmemes = new mememe("memes", ":O" , "img/Damk memes.jpg");
 var pingas = new mememe("sexy", "PINGAS", "img/Pingas.png");
